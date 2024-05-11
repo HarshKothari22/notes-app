dbc0d741f399779fd4ec9829a9110cbe52c25854
HarshKothari22 <164483007+HarshKothari22@users.noreply.github.com>
2024-05-11 22:50:10 +0530

    Add files via upload

---
+++ notes-app/js/app.js
@@ -0,0 +1,80 @@
+const addBtn = document.querySelector("#addBtn");
+const main = document.querySelector("#main");
+
+addBtn.addEventListener("click",function(){
+    addNote();
+});
+
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div class="tool">
+        <i class="save fa-solid fa-floppy-disk"></i>
+        <i class="trash fa-solid fa-trash"></i>
+    </div>
+    <textarea>${text}</textarea>
+`;
+
+note.querySelector(".trash").addEventListener("click", function(){
+    note.remove();
+    saveNotes();
+});
+
+note.querySelector(".save").addEventListener("click",function(){
+    saveNotes();
+});
+
+note.querySelector("textarea").addEventListener("focusout",function(){
+        saveNotes();
+});
+
+main.appendChild(note);
+}
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    // console.log(notes);
+    const data = [];
+
+    notes.forEach((note) => {
+        data.push(note.value);
+        // console.log(data);
+    });
+
+    if(data.length === 0){
+        localStorage.removeItem("notes");
+    }else{
+        localStorage.setItem("notes", JSON.stringify(data));
+    }
+    
+}
+
+(
+    function(){
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+
+        if(lsNotes === null){
+            addNote();
+        }else{
+            lsNotes.forEach((lsNote) => {
+                    addNote(lsNote);
+                }
+            )
+        }
+    }
+)();
+
+
+// //------------------------------------------------------------------------------------------------
+// ------------------    code for making an element, writing its class, and adding value to element and printing it    --------------------------
+// let h1 = document.createElement('div');
+// let adgf = document.getElementById("main");
+// let text = document.createTextNode('hello world');
+// h1.appendChild(text);
+// h1.classList.add("asdg");
+// adgf.appendChild(h1);
+// //------------------------------------------------------------------------------------------------
+
+
+
